5690679c8736baa8326e67118d5f6ce8a4d03ffc
Scrabbler <wwww.github.com>
2017-03-26 19:30:05 +0530

    Eigtheenth commit

---
+++ Register_Login_CodeCourse/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+      <title>Website Title</title>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" href="css/screen.css" />
+  </head>
+  <body>
+    <header>
+      <h1 class="logo">Logo</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <div class="clear">
+
+      </div>
+    </header>
+    <div id="container">
+      <aside>
+        <div class="widget">
+          <h2>Widget Header</h2>
+          <div class="inner">
+            Widget Contents
+          </div>
+        </div>
+      </aside>
+    </div>
+    <footer>
+      &copy; phpacademy.org 2011. All rights reserved.
+    </footer>
+  </body>
+</html>
