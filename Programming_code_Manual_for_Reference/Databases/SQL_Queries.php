d1c262c5c909c287e5687acb6198c6d1d97999c6
UtileHomme <jatins368@gmail.com>
2017-09-20 08:44:33 +0530

    PHP codes changed

--- Programming_code_Manual_for_Reference/Databases/SQL_Queries.php
+++ Programming_code_Manual_for_Reference/Databases/SQL_Queries.php
@@ -48,4 +48,46 @@ SELECT state,city,name FROM customers ORDER BY state,name
 SELECT name, id FROM customers ORDER BY id DESC
 //use LIMIT to get highest or lowest
 
+// How to select particular column values using "where" clause
+SELECT `username` , `password` FROM users where `id`=1
+
+SELECT `firstname` , `surname` FROM users where `username`='alex' AND `password`='password'
+
+//How to update a particular row using "where" clause
+UPDATE `users` SET `firstname`='Dale' where `id`=1
+
+//How to delete a particular row using "where" clause
+DELETE FROM `users` where `id`=1
+
+// How to insert into a particular table some row
+INSERT INTO `users` VALUES('','alex','password','Alex','Pumpkin')
+
+//How to arrange records in a particular order
+SELECT `firstname`, `surname` FROM `users` ORDER BY `id` DESC
+
+// How to use JOINS in SQL
+
+//returns the data based on the matching condition between two tables and Null is printed for the Right table when data doesn't exists
+SELECT `people`.`name`, `pets`.`pet` FROM `people` LEFT JOIN `pets` ON `people`.`id`=`pets`.`people_id`
+
+//displays data (NULL for no match found) for all data which is matched
+SELECT `people`.`name`, `pets`.`pet` FROM `people` RIGHT JOIN `pets` ON `people`.`id`=`pets`.`people_id`
+
+//displays data only where matching condition is met
+SELECT `people`.`name`, `pets`.`pet` FROM `people` JOIN `pets` ON `people`.`id`=`pets`.`people_id`
+
+// How to return data using "LIKE" and "NOT LIKE"
+
+//Return all names have surname as garrett
+SELECT `name` FROM `names` WHERE `name` LIKE '%garrett'
+
+//Returns the names not having 'g' inside it
+SELECT `name` FROM `names` WHERE `name` NOT LIKE '%g%'
+
+// How to select distinct data from a table
+
+//return distinct names
+SELECT DISTINCT `surname` FROM `peoples`
+SELECT DISTINCT `firstname`, `surname` FROM `peoples`
+
  ?>
--- Programming_code_Manual_for_Reference/PHP/All_about_SQL_in_PHP.php
+++ Programming_code_Manual_for_Reference/PHP/All_about_SQL_in_PHP.php
@@ -1,3 +1,27 @@
+<!-- How to connect to a database server in PHP -->
+
+<?php
+// print_r(PDO::getAvailableDrivers());     //to check the available drivers
+
+$servername="localhost";
+$user = 'root';
+$password= 'jatin';
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=testing", $user, $password);
+
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //  var_dump($conn);
+    // set the PDO error mode to exception
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+?>
+
+
 <!-- How to write an update query in core PHP -->
 
 UPDATE `table_name`
@@ -7,6 +31,49 @@ UPDATE `table_name`
 <!-- How to write SELECT Query using "LIKE" -->
 
 SELECT `place` , `description` FROM `places` WHERE `place` LIKE ?
+
+<!-- How to retrieve data using PDO and bindParam -->
+
+$query = "SELECT `id` FROM `users` WHERE `username`=:username AND `password`=:password ";
+
+$result = $conn->prepare($query); //helps avoid sql injection
+$result->bindParam(':username', $username, PDO::PARAM_STR); //putting parameters in place of actual data
+$result->bindParam(':password', $password_hash, PDO::PARAM_STR);
+$result->execute();
+$num_of_rows = $result->rowCount(); //this will count the rows affected in the last execution of the query
+//which are returned after executing the sql statement
+
+if($num_of_rows==0)
+// password and username combination don't match
+{
+  echo 'Invalid username/password';
+}
+else if($num_of_rows==1)
+{
+  $user_id =  $result->fetchColumn();   //this will return the column value(id) of the logged in user
+  $_SESSION['user_id']=$user_id;    //store this into a session
+  header('Location: video136.php');   //redirect the page to original page
+}
+
+<!-- How to retrieve data using "integer value" -->
+
+/  $query1 = "SELECT ".implode(' ', $fields)." FROM `users` WHERE `id`=".intval($_SESSION['user_id']);
+$query1 = "SELECT $field FROM `users` WHERE `id`=".intval($_SESSION['user_id']);
+
+//this query will retrieve the field(column) value for the user id - (the user_id of the user logged in)
+//var_dump($query1);
+
+$result = $conn->query($query1);
+$result->setFetchMode(PDO::FETCH_OBJ);
+
+//var_dump($result);
+
+$a = $result->fetch();
+//  var_dump($a);
+
+return $a->$field;
+//var_dump(implode(',', (array) $a));
+//return (implode(',', (array) $a));
 
 <!-- How to retrieve result from query and display all results -->
 
@@ -31,3 +98,17 @@ SELECT `place` , `description` FROM `places` WHERE `place` LIKE ?
         {
             echo '<strong>'.$row['place'].'</strong><br /><br />';
         }
+
+<!-- How to insert data into table using "PDO" and "bindParam" -->
+
+$query3 = "INSERT INTO users(username, password, firstname, surname)  VALUES(:username1, :password1, :firstname1, :surname1) ";
+//var_dump($query3);
+$result3 = $conn->prepare($query3);
+//var_dump($result3);
+
+$result3->bindParam(':username1',$username,PDO::PARAM_STR);
+$result3->bindParam(':password1',$password_hash,PDO::PARAM_STR);
+$result3->bindParam(':firstname1',$firstname,PDO::PARAM_STR);
+$result3->bindParam(':surname1',$surname,PDO::PARAM_STR);
+$a = $result3->execute();
+//var_dump($a);
