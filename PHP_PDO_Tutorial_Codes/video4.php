7679589563e9bce993279e54108fdeabc871b973
Jatin Sharma <jatinsharma@Jatins-MacBook-Air.local>
2020-06-05 07:10:23 +0530

    comment

--- PHP_PDO_Tutorial_Codes/video4.php
+++ PHP_PDO_Tutorial_Codes/video4.php
@@ -16,7 +16,7 @@ $query = $conn->query('SELECT * from users');
 
 $query->setFetchMode(PDO::FETCH_CLASS, 'UserDetails');
 // Set fetch mode
-
+//hello
 while($r = $query->fetch())
 {
   echo $r->entry.'<br />';
